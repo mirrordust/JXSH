4e3e31a30189c7f6da02cedd722c4b7a3e74aca5
mirrordust <shhliao754@outlook.com>
2021-09-29 09:55:17 +0800

    WIP

--- wa/src/App.js
+++ wa/src/App.js
@@ -15,32 +15,34 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 import { UsersList } from './features/users/UsersList'
 import { UserPage } from './features/users/UserPage'
 import { NotificationsList } from './features/notifications/NotificationsList'
+import MyEditor from './features/posts/Editor';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <React.Fragment>
-                <AddPostForm />
-                <PostsList />
-              </React.Fragment>
-            )}
-          />
-          <Route exact path="/posts/:postId" component={SinglePostPage} />
-          <Route exact path="/editPost/:postId" component={EditPostForm} />
-          <Route exact path="/users" component={UsersList} />
-          <Route exact path="/users/:userId" component={UserPage} />
-          <Route exact path="/notifications" component={NotificationsList} />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
+    <MyEditor />
+    // <Router>
+    //   <Navbar />
+    //   <div className="App">
+    //     <Switch>
+    //       <Route
+    //         exact
+    //         path="/"
+    //         render={() => (
+    //           <React.Fragment>
+    //             <AddPostForm />
+    //             <PostsList />
+    //           </React.Fragment>
+    //         )}
+    //       />
+    //       <Route exact path="/posts/:postId" component={SinglePostPage} />
+    //       <Route exact path="/editPost/:postId" component={EditPostForm} />
+    //       <Route exact path="/users" component={UsersList} />
+    //       <Route exact path="/users/:userId" component={UserPage} />
+    //       <Route exact path="/notifications" component={NotificationsList} />
+    //       <Redirect to="/" />
+    //     </Switch>
+    //   </div>
+    // </Router>
   )
 }
 
