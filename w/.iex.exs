a566e1c9257b5b19f023e51851e5d0e13975da04
mirrordust <shhliao754@outlook.com>
2021-11-13 14:52:44 +0800

    WIP

--- w/.iex.exs
+++ w/.iex.exs
@@ -1,2 +1,2 @@
-IEx.configure(colors: [enabled: true])
-Application.put_env(:elixir, :ansi_enabled, true)
+# IEx.configure(colors: [enabled: true])
+# Application.put_env(:elixir, :ansi_enabled, true)
--- w/lib/w/release.ex
+++ w/lib/w/release.ex
@@ -11,6 +11,7 @@ defmodule W.Release do
   end
 
   def createdb do
+    IO.puts("=== Start createdb... ===")
     load_app()
 
     Enum.each(@start_apps, &Application.ensure_all_started/1)
@@ -18,6 +19,8 @@ defmodule W.Release do
     for repo <- repos() do
       :ok = ensure_repo_created(repo)
     end
+
+    IO.puts("=== createdb DONE. ===")
   end
 
   defp ensure_repo_created(repo) do
@@ -31,6 +34,7 @@ defmodule W.Release do
   end
 
   def migrate do
+    IO.puts("=== Start migrate... ===")
     load_app()
 
     Enum.each(@start_apps, &Application.ensure_all_started/1)
@@ -38,6 +42,8 @@ defmodule W.Release do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+
+    IO.puts("=== migrate DONE. ===")
   end
 
   def migrate1 do
@@ -67,5 +73,45 @@ defmodule W.Release do
     load_app()
     Application.put_env(@app, :minimal, true)
     Application.ensure_all_started(@app)
+  end
+
+  def seed do
+    alias W.Auth.User
+    alias W.Repo
+
+    random_string = fn length ->
+      :crypto.strong_rand_bytes(length)
+      |> Base.url_encode64()
+      |> binary_part(0, length)
+    end
+
+    email =
+      System.get_env("SEEDS_USER_EMAIL") ||
+        raise """
+        environment variable SEEDS_USER_EMAIL is missing.
+        """
+
+    password =
+      System.get_env("SEEDS_USER_PASSWORD") ||
+        raise """
+        environment variable SEEDS_USER_PASSWORD is missing.
+        """
+
+    name =
+      System.get_env("SEEDS_USER_NAME") ||
+        random_string.(10)
+
+    username =
+      System.get_env("SEEDS_USER_USERNAME") ||
+        "admin_" <> random_string.(5)
+
+    %User{}
+    |> User.changeset(%{
+      name: name,
+      username: username,
+      email: email,
+      password: password
+    })
+    |> Repo.insert!()
   end
 end
