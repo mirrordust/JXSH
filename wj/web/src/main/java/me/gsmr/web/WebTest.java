5b4c68ad923adcba2dcae4ba6cf02f1a0f35a46c
mirrordust <shhliao754@outlook.com>
2021-12-11 21:01:26 +0800

    WIP

--- wj/web/src/main/java/me/gsmr/web/WebTest.java
+++ wj/web/src/main/java/me/gsmr/web/WebTest.java
@@ -1,15 +1,38 @@
 package me.gsmr.web;
 
-import me.gsmr.dao.DaoTest;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RefreshScope
 @RestController
 @RequestMapping("test")
 public class WebTest {
+
+    private String name;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Value("${message:Hello default}")
+    private String message;
+
     @RequestMapping("showAll")
     public String showAll() {
-        DaoTest daoTest = new DaoTest();
-        return daoTest.showDao() + "  Web! ";
+        return " From Web! " + message;
     }
+
+    @RequestMapping("/service-instance/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
 }
